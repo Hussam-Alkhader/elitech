f14e84222a8d2649c9d23dd9c12b586c453e7a7b
Hussam-Alkhader <hussam@gmail.com>
2024-11-18 17:04:16 +0300

    edites

--- footer-component.js
+++ footer-component.js
@@ -6,13 +6,13 @@ class FooterComponent extends HTMLElement {
         <div class="footer__shape-regular-left wow slideInLeft" data-wow-delay="00ms" data-wow-duration="1500ms">
             <img src="assets/images/shape/footer-regular-left.png" alt="shape">
         </div>
-        <div class="footer__shape-solid-left wow slideInLeft" data-wow-delay="200ms" data-wow-duration="1500ms">
+        <div class="footer__shape-solid-left wow slideInLeft" data-wow-delay="00ms" data-wow-duration="1500ms">
             <img class="sway_Y__animation" src="assets/images/shape/footer-solid-left.png" alt="shape">
         </div>
         <div class="footer__shape-solid-right wow slideInRight" data-wow-delay="00ms" data-wow-duration="1500ms">
             <img class="sway_Y__animation" src="assets/images/shape/footer-regular-right.png" alt="shape">
         </div>
-        <div class="footer__shape-regular-right wow slideInRight" data-wow-delay="200ms" data-wow-duration="1500ms">
+        <div class="footer__shape-regular-right wow slideInRight" data-wow-delay="00ms" data-wow-duration="1500ms">
             <img src="assets/images/shape/footer-solid-right.png" alt="shape">
         </div>
         <div class="footer__shadow-shape">
